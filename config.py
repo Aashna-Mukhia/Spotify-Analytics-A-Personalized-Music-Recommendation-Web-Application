a894f472aaee2be9329c61c1b24f6eb3f23c70c7
aayushkapudasaini12-crypto <aayushkapudasaini12@gmail.com>
2026-06-14 00:39:33 +0545

    Update app and config from main

--- config.py
+++ config.py
@@ -10,8 +10,10 @@ class Config:
     SECRET_KEY: str = os.getenv("FLASK_SECRET_KEY", "dev-secret-key")
     SPOTIFY_CLIENT_ID: str = os.getenv("SPOTIFY_CLIENT_ID", "")
     SPOTIFY_CLIENT_SECRET: str = os.getenv("SPOTIFY_CLIENT_SECRET", "")
-    SPOTIFY_REDIRECT_URI: str = os.getenv("SPOTIFY_REDIRECT_URI", "http://127.0.0.1:5000/callback")
-    SPOTIFY_DEFAULT_TIME_RANGE: str = os.getenv("SPOTIFY_DEFAULT_TIME_RANGE", "medium_term")
+    SPOTIFY_REDIRECT_URI: str = os.getenv(
+        "SPOTIFY_REDIRECT_URI", "http://127.0.0.1:5000/callback")
+    SPOTIFY_DEFAULT_TIME_RANGE: str = os.getenv(
+        "SPOTIFY_DEFAULT_TIME_RANGE", "medium_term")
     SPOTIFY_AUTH_URL: str = "https://accounts.spotify.com/authorize"
     SPOTIFY_TOKEN_URL: str = "https://accounts.spotify.com/api/token"
     SPOTIFY_API_BASE: str = "https://api.spotify.com/v1"
